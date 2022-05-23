c8efd60a77c39db1a7ebcd4badba0f4cbb08b1e7
Gabriel Romero <gabriel.romero@utec.edu.pe>
2022-05-22 22:26:00 -0500

    Añadimos el menu con el input de opcion

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 #!/usr/bin/env python 
 #Archivo Principal de Ejecuación
-MIRA ESTE CAMBIO
+
+menu = """
+VENTA AUTOS
+1. Registrar auto
+2. Autos disponibles
+3. Compra auto
+"""
+
+print(menu)
+int(input("opcion: "))
