9f7829a5e519054b3a317c7f8d373d2475558865
Sherlock 3 <104222694+SherlockPE@users.noreply.github.com>
2022-06-05 01:42:31 -0500

    Actualización pandas 1.1

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-#!/usr/bin/env python 
 #Archivo Principal de Ejecuación 
 import modulos
 
-menu = """
-VENTA AUTOS
-1. Registrar auto
-2. Autos disponibles
-3. Compra auto
-"""
-print(menu)
+with open("menu.txt", "r") as abrirMenu:
+    print(abrirMenu.read())
 
 modulos.menu_opcion()
