970d9fc0c4673303934c2cf14bf0e09a7f72b418
SherlockPE <fabricio.lopez@utec.edu.pe>
2022-06-17 15:58:57 -0500

    uwunt

--- modulos.py
+++ modulos.py
@@ -36,13 +36,6 @@ def registro_auto():
         fabricacion = int(input("Introduce el año de Fabricación: "))
         color = input("Introduce el Color: ")
         precio = int(input("Introduce el Precio: "))
-        while True:
-            lista = []
-            lista.append(precio)
-            if precio[0] == "-":
-                precio = int(input("Introduce el Precio: "))
-            else:
-                break
 
     except:
         print("--------------------------------------------------------------------")
