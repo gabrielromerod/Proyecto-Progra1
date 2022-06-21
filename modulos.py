6725ab1c104f084eaa17273fdf47e4a22d729778
Gabriel Romero <gabriel.romero@utec.edu.pe>
2022-06-21 14:42:50 -0500

    completo

--- modulos.py
+++ modulos.py
@@ -13,7 +13,6 @@ def menu_opcion():
             enterContinuar()
             break
         elif opcion == "2":
-            cambiar_orden("registro.txt")
             inventario_auto()
             enterContinuar()
             break
@@ -78,7 +77,7 @@ def registro_auto():
 
 #Mostrar el inventario de la tienda(los autos disponibles)      
 def inventario_auto():
-    try:
+    if os.path.exists("registro.txt"):
         cambiar_orden("registro.txt")
         with open("registro.txt", "r") as lineas:
             listaLineas = []
@@ -92,9 +91,10 @@ def inventario_auto():
             
             #Imprimo la tablita
             print(tabulate(matrisita, headers = ["","Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
-    except:
+    else:
         print("No hay autos registrados")
-        
+        enterContinuar()
+
 def inventario_auto_vendido():
     if os.path.exists("vendidos.txt"):
         cambiar_orden("vendidos.txt")
@@ -112,6 +112,7 @@ def inventario_auto_vendido():
             print(tabulate(matrisita, headers = ["","Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
     else:
         print("No hay autos vendidos")
+        enterContinuar()
 
 #Filtro de selección para comprar auto
 def filtrar_por(dato, matriz_ingresada, posicion):
@@ -129,46 +130,50 @@ def filtrar_por(dato, matriz_ingresada, posicion):
     return nueva_matriz
 
 def comprar_auto():
-    #Mostrar título de compra de autos
-    titulos.compra_de_autos()
+    if os.path.exists("registro.txt"):
+        #Mostrar título de compra de autos
+        titulos.compra_de_autos()
 
-    #Creación de la nueva matriz que se encargará de los otros datos
-    with open("registro.txt", "r") as lineas:
-        listaLineas = []
-        listaLineas = (lineas.readlines())
-        matrisita = []
-        for i in listaLineas:
-            lineax = i.split(",")
-            matrisita.append(lineax)
+        #Creación de la nueva matriz que se encargará de los otros datos
+        with open("registro.txt", "r") as lineas:
+            listaLineas = []
+            listaLineas = (lineas.readlines())
+            matrisita = []
+            for i in listaLineas:
+                lineax = i.split(",")
+                matrisita.append(lineax)
 
-    #Filtrar datos
-    #marca
-    marca_seleccionada = input("Introduce la marca del auto que deseas comprar: ")
-    marca_seleccionada = marca_seleccionada.lower().capitalize()
-    lista_de_auto_a_comprar = filtrar_por(marca_seleccionada, matrisita, 1)
-    print(tabulate(lista_de_auto_a_comprar, headers = ["Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
-    si_queda_uno(lista_de_auto_a_comprar)
+        #Filtrar datos
+        #marca
+        marca_seleccionada = input("Introduce la marca del auto que deseas comprar: ")
+        marca_seleccionada = marca_seleccionada.lower().capitalize()
+        lista_de_auto_a_comprar = filtrar_por(marca_seleccionada, matrisita, 1)
+        print(tabulate(lista_de_auto_a_comprar, headers = ["Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
+        si_queda_uno(lista_de_auto_a_comprar)
 
-    #Fabricación
-    fabricacion_seleccionada = input("Introduce el año de fabricación del auto que deseas comprar: ")
-    fabricacion_seleccionada = fabricacion_seleccionada.lower().capitalize() 
-    lista_de_auto_a_comprar = filtrar_por(fabricacion_seleccionada, lista_de_auto_a_comprar, 2)
-    print(tabulate(lista_de_auto_a_comprar, headers = ["", "Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
-    si_queda_uno(lista_de_auto_a_comprar)
+        #Fabricación
+        fabricacion_seleccionada = input("Introduce el año de fabricación del auto que deseas comprar: ")
+        fabricacion_seleccionada = fabricacion_seleccionada.lower().capitalize() 
+        lista_de_auto_a_comprar = filtrar_por(fabricacion_seleccionada, lista_de_auto_a_comprar, 2)
+        print(tabulate(lista_de_auto_a_comprar, headers = ["", "Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
+        si_queda_uno(lista_de_auto_a_comprar)
 
-    #Color
-    color_seleccionado = input("Introduce el color seleccionado del auto que deseas comprar: ")
-    color_seleccionado = color_seleccionado.lower().capitalize()
-    lista_de_auto_a_comprar = filtrar_por(color_seleccionado, lista_de_auto_a_comprar, 3)
-    print(tabulate(lista_de_auto_a_comprar, headers = ["", "Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
-    si_queda_uno(lista_de_auto_a_comprar)
+        #Color
+        color_seleccionado = input("Introduce el color seleccionado del auto que deseas comprar: ")
+        color_seleccionado = color_seleccionado.lower().capitalize()
+        lista_de_auto_a_comprar = filtrar_por(color_seleccionado, lista_de_auto_a_comprar, 3)
+        print(tabulate(lista_de_auto_a_comprar, headers = ["", "Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
+        si_queda_uno(lista_de_auto_a_comprar)
 
-    #Precio
-    precio_seleccionado = input("Introduce el precio seleccionado del auto que deseas comprar: ")
-    precio_seleccionado = precio_seleccionado.lower().capitalize()
-    lista_de_auto_a_comprar = filtrar_por(precio_seleccionado, lista_de_auto_a_comprar, 4)
-    print(tabulate(lista_de_auto_a_comprar, headers = ["", "Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
-    si_queda_uno(lista_de_auto_a_comprar)
+        #Precio
+        precio_seleccionado = input("Introduce el precio seleccionado del auto que deseas comprar: ")
+        precio_seleccionado = precio_seleccionado.lower().capitalize()
+        lista_de_auto_a_comprar = filtrar_por(precio_seleccionado, lista_de_auto_a_comprar, 4)
+        print(tabulate(lista_de_auto_a_comprar, headers = ["", "Marca", "Fabricacion", "Color", "Precio", "Estado"], tablefmt="fancy_grid" ))
+        si_queda_uno(lista_de_auto_a_comprar)
+    else:
+        print("No hay autos registrados")
+        enterContinuar()
 
 def reemplazar_vendido(file, x):
     temporal = []
